73979ebc446823c1896171e1cbf9a09592b5ad32
Matheus <mgabryel2007@gmail.com>
2025-04-14 20:28:01 -0300

    feat: implement habit page layout

--- src/app/pages/habit/habit.page.ts
+++ src/app/pages/habit/habit.page.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './../../components/menu/menu.component';
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonContent, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
-
+import { HeaderComponent } from "../../components/header/header.component";
+import { CreateCardComponent } from "../../components/create-card/create-card.component";
 @Component({
   selector: 'app-habit',
   templateUrl: './habit.page.html',
   styleUrls: ['./habit.page.scss'],
   standalone: true,
-  imports: [IonRow, IonCol, IonGrid, IonContent, MenuComponent, CommonModule]
+  imports: [IonRow, IonCol, IonGrid, IonContent, MenuComponent, CommonModule, HeaderComponent, CreateCardComponent]
 })
 export class HabitPage {
 
