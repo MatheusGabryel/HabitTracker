e419f56616f685359594da4e308924a6cd263852
Matheus <mgabryel2007@gmail.com>
2025-04-17 16:06:13 -0300

    feat: update habit page layout

--- src/app/pages/habit/habit.page.ts
+++ src/app/pages/habit/habit.page.ts
@@ -4,14 +4,21 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonContent, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
 import { HeaderComponent } from "../../components/header/header.component";
 import { CreateCardComponent } from "../../components/create-card/create-card.component";
+import { HabitCardComponent } from "../../components/habit-card/habit-card.component";
 @Component({
   selector: 'app-habit',
   templateUrl: './habit.page.html',
   styleUrls: ['./habit.page.scss'],
   standalone: true,
-  imports: [IonRow, IonCol, IonGrid, IonContent, MenuComponent, CommonModule, HeaderComponent, CreateCardComponent]
+  imports: [IonRow, IonCol, IonGrid, IonContent, MenuComponent, CommonModule, HeaderComponent, CreateCardComponent, HabitCardComponent]
 })
 export class HabitPage {
+  tabs = ['Ver tudo', 'Lista 1', 'Lista 2'];
+  activeTab = 'Ver tudo';
+
+  setActive(tab: string) {
+    this.activeTab = tab;
+  }
 
   constructor() { }
 
