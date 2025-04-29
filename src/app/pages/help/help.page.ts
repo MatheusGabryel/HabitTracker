7f71b3b3f9a770a5a200bed8bda41b02c37d57dd
Matheus <mgabryel2007@gmail.com>
2025-04-29 17:34:31 -0300

    feat: implement help layout

--- src/app/pages/help/help.page.ts
+++ src/app/pages/help/help.page.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './../../components/menu/menu.component';
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonContent, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-help',
   templateUrl: './help.page.html',
   styleUrls: ['./help.page.scss'],
   standalone: true,
-  imports: [IonRow, IonCol, IonGrid, IonContent, MenuComponent, CommonModule]
+  imports: [IonRow, IonCol, IonGrid, IonContent, MenuComponent, CommonModule, HeaderComponent]
 })
 export class HelpPage {
 
