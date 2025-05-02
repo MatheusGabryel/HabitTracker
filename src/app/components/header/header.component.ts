9bb166e71b49860f015f92b50283f209ec970f3b
Matheus <mgabryel2007@gmail.com>
2025-05-02 16:41:16 -0300

    feat: implement account modal and user menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,41 +1,107 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuService } from 'src/app/services/menu.service';
+import { Component, HostListener, ElementRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { addIcons } from 'ionicons';
-import { menuOutline, } from 'ionicons/icons';
+import { Router, RouterLink, NavigationEnd } from '@angular/router';
 import { IonIcon } from "@ionic/angular/standalone";
+import { AlertController } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { menuOutline } from 'ionicons/icons';
+import { AuthService } from 'src/app/services/auth.service';
+import { MenuService } from 'src/app/services/menu.service';
+import { UserService } from 'src/app/services/user.service';
+import { ProfileModalComponent } from '../../components/profile-modal/profile-modal.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [IonIcon, CommonModule]
+  imports: [IonIcon, CommonModule, RouterLink, ProfileModalComponent]
 })
 export class HeaderComponent {
 
+  private menuService = inject(MenuService);
+  private alertController = inject(AlertController);
+  private authService = inject(AuthService);
+  private userService = inject(UserService);
+  private elementRef = inject(ElementRef);
+  private router = inject(Router);
+
+  public nome: string = '';
+  public email: string = '';
+  public isModalOpen: boolean = false;
+  public isOpen: boolean = false;
+  public isMobile: boolean = false;
+  public menuOpen: boolean = false;
+  public routerName: string = '';
+  public pageTitle: string = '';
   public routeToTitleMap: { [key: string]: string } = {
     'home': 'Início',
     'habit': 'Hábitos',
-    'categories': 'Categorias',
     'goals': 'Metas',
     'settings': 'Configurações',
     'help': 'Ajuda',
     'profile': 'Perfil',
   };
-  public routerName: string = '';
-  public pageTitle: string = '';
-  menuOpen = false;
-  isMobile = false;
-
-  constructor(public menuService: MenuService) {
+  constructor(
+  ) {
     addIcons({ menuOutline });
     this.routerName = window.location.pathname.split('/')[1];
     this.pageTitle = this.routeToTitleMap[this.routerName] || 'App';
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.closeUserMenu();
+      }
+    });
   }
+
+  ngOnInit() {
+    this.userService.getUserId().then((uid) => {
+      if (uid) {
+        this.userService.getUserDoc(uid).then(data => {
+          if (data) {
+            this.nome = data.displayName;
+            this.email = data.email;
+          }
+        });
+      }
+    });
+  }
+
   toggleMenu() {
     this.menuService.toggleMenu();
   }
 
-  ngOnInit() { }
+  toggleUserMenu() {
+    this.isOpen = !this.isOpen;
+  }
+  closeUserMenu() {
+    this.isOpen = false;
+  }
 
+  async logout() {
+    const alert = await this.alertController.create({
+      header: 'Confirmação',
+      message: 'Deseja realmente sair da conta?',
+      buttons: [
+        { text: 'Cancelar', role: 'cancel' },
+        {
+          text: 'Sair',
+          handler: async () => {
+            await this.authService.logout();
+            this.router.navigate(['/login']);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+    if (!clickedInside && this.isOpen) {
+      this.closeUserMenu();
+    }
+  }
 }
