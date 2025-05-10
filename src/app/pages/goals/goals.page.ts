55778af7d56556f042383b71aff33db74199c49f
Matheus <mgabryel2007@gmail.com>
2025-05-09 23:53:09 -0300

    feat: implement goal page layout

--- src/app/pages/goals/goals.page.ts
+++ src/app/pages/goals/goals.page.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './../../components/menu/menu.component';
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonContent, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
+import { HeaderComponent } from "../../components/header/header.component";
+import { CreateCardComponent } from "../../components/create-card/create-card.component";
 
 @Component({
   selector: 'app-goals',
   templateUrl: './goals.page.html',
   styleUrls: ['./goals.page.scss'],
   standalone: true,
-  imports: [IonRow, IonCol, IonGrid, IonContent, MenuComponent, CommonModule]
+  imports: [IonRow, IonCol, IonGrid, IonContent, MenuComponent, CommonModule, HeaderComponent, CreateCardComponent]
 })
 export class GoalsPage {
 
