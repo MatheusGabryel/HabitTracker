1a336394c8e3c9cffd7ecbe614c6bf7092870e3f
Matheus Gabryel <mgabryel2007@gmail.com>
2025-05-14 16:51:01 -0300

    feat: implement home page layout

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,35 +1,22 @@
 import { MenuComponent } from './../../components/menu/menu.component';
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonContent, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
 import { AuthService } from 'src/app/services/auth.service';
 import { CommonModule } from '@angular/common';
 import { Observable, switchMap, of } from 'rxjs';
+import { HeaderComponent } from "../../components/header/header.component";
 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonRow, IonCol, IonGrid, IonContent, MenuComponent, CommonModule],
+  imports: [IonRow, IonCol, IonGrid, IonContent, MenuComponent, CommonModule, HeaderComponent],
 })
 export class HomePage {
-  userData: any;;
-  // user$ = this.authService.getUser();
-  displayName = '';
-  email = ''
-  userData$!: Observable<any>;
 
   constructor(private authService: AuthService) {
   }
   ngOnInit() {
-    // this.userData$ = this.authService.getUser().pipe(
-    //   switchMap((user) => {
-    //     if (user) {
-    //       return this.authService.getUserDataFromFirestore$(user.uid);
-    //     } else {
-    //       return of(null);
-    //     }
-    //   })
-    // );
   }
 }
