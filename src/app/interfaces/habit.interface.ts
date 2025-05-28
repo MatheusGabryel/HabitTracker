3eaf00c13f5c10af0596dc02361118456106afa9
Matheus <mgabryel2007@gmail.com>
2025-05-28 17:11:01 -0300

    feat: extend Habit interface

--- src/app/interfaces/habit.interface.ts
+++ src/app/interfaces/habit.interface.ts
@@ -1,12 +1,24 @@
 export interface HabitData {
     id: string,
-    nome: string,
-    categoria: string,
-    duracao?: string,
-    horaInicio?: string,
-    horaFim?: string,
-    vezesPorDia?: string,
-    dias: string[],
-    descricao?: string,
-    prioridade: string,
+    name: string,
+    category: string,
+    days: string[],
+    description?: string,
+    priority: string,
+    progressType: 'yes_no' | 'time' | 'times',
+
+    timesTarget?: {
+        value: number;
+        rule: 'equal' | 'at_least' | 'at_most' | 'any';
+    },
+    timeTarget?: {
+        hours?: number;
+        minutes?: number;
+        seconds?: number;
+        rule?: 'equal' | 'at_least' | 'at_most' | 'any';
+    },
+    state: 'in_progress' | 'completed' | 'not_completed';
+    currentTimes?: number;
+    inputValue?: number;
+    progressValue: number;
 }
