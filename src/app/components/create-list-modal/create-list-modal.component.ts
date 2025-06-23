037b3d72c1ce55acdfe454678d748bf026f90f26
Matheus <mgabryel2007@gmail.com>
2025-06-23 20:22:44 -0300

    refactor: remove unused field from habitlist object

--- src/app/components/create-list-modal/create-list-modal.component.ts
+++ src/app/components/create-list-modal/create-list-modal.component.ts
@@ -25,7 +25,6 @@ export class CreateListModalComponent implements OnInit {
     name: '',
     createdAt: serverTimestamp(),
     updatedAt: serverTimestamp(),
-    habitIds: [],
     categories: [] as string[],
   }
 
