0af60429b9b58be61ab08bffbd9a4e3d6a0126c6
Matheus <mgabryel2007@gmail.com>
2025-07-07 18:28:42 -0300

    chore: configure Firebase Hosting for app deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
-  production: true
-  
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyAMei6WLAcfQiVwvk9VAT8Zw974by2phBE",
+    authDomain: "habittracker-a4c2d.firebaseapp.com",
+    projectId: "habittracker-a4c2d",
+    storageBucket: "habittracker-a4c2d.appspot.com",
+    messagingSenderId: "21491092942",
+    appId: "1:21491092942:web:1224b480da7cf378562f9f",
+    measurementId: "G-5ZKH74VVGP"
+  }
 };
