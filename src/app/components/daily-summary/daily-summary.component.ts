b60c9754b67af6a568214831f928b18ab319ed6c
Matheus <mgabryel2007@gmail.com>
2025-07-28 18:46:49 -0300

    style: update style daily summary component

--- src/app/components/daily-summary/daily-summary.component.ts
+++ src/app/components/daily-summary/daily-summary.component.ts
@@ -1,16 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { ApexNonAxisChartSeries, ApexChart, ApexResponsive, ApexLegend, NgApexchartsModule } from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  legend: ApexLegend;
+};
 
 @Component({
   selector: 'app-daily-summary',
   templateUrl: './daily-summary.component.html',
   styleUrls: ['./daily-summary.component.scss'],
+  imports: [NgApexchartsModule],
 })
-export class DailySummaryComponent  implements OnInit {
+export class DailySummaryComponent implements OnInit {
+  chartOptions: ChartOptions = {
+    series: [4, 3],  
+    chart: {
+      width: 380,
+      type: 'pie',
+    },
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 300
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }
+    ],
+    legend: {
+      position: 'right',
+      offsetY: 0,
+      height: 230,
+    },
+  };
 
   public todayDate: string;
-  ngOnInit() {}
+  ngOnInit() { }
 
-    constructor() {
+  constructor() {
     const now = new Date();
     const weekDay = now.toLocaleDateString('pt-BR', { weekday: 'short' }).replace('.', '');
     const dayMonth = now.toLocaleDateString('pt-BR', { day: '2-digit', month: 'short' });
