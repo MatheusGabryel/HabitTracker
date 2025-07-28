52b3e17e4cff8dba7a9fd046d116c407fcdf4eeb
Matheus <mgabryel2007@gmail.com>
2025-07-28 18:48:33 -0300

    style: update home page with new components

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,13 +6,15 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "../../components/header/header.component";
 import { DashboardDaySwiperComponent } from "src/app/components/dashboard-day-swiper/dashboard-day-swiper.component";
 import { DailySummaryComponent } from "src/app/components/daily-summary/daily-summary.component";
+import { DashboardGoalsComponent } from "src/app/components/dashboard-goals/dashboard-goals.component";
+import { DashboardStatsComponent } from "src/app/components/dashboard-stats/dashboard-stats.component";
 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonContent, MenuComponent, CommonModule, HeaderComponent, DashboardDaySwiperComponent, DailySummaryComponent],
+  imports: [IonContent, MenuComponent, CommonModule, HeaderComponent, DashboardDaySwiperComponent, DailySummaryComponent, DashboardGoalsComponent, DashboardStatsComponent],
 })
 export class HomePage {
 
