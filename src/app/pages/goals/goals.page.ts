8053a0087af469a457f2aef3a3d82c79fd6fe8f4
Matheus <mgabryel2007@gmail.com>
2025-08-25 17:14:29 -0300

    feat: integrates edit-goal-modal and filters-goal-modal in goal page

--- src/app/pages/goals/goals.page.ts
+++ src/app/pages/goals/goals.page.ts
@@ -7,17 +7,21 @@ import { HeaderComponent } from "../../components/header/header.component";
 import { CreateGoalModalComponent } from "../../components/create-goal-modal/create-goal-modal.component";
 import { animate, style, transition, trigger } from '@angular/animations';
 import { UserService } from 'src/app/services/user/user.service';
-import { GoalData } from 'src/app/interfaces/goal.interface';
+import { GoalData, GoalType, StateGoalType } from 'src/app/interfaces/goal.interface';
 import Swal from 'sweetalert2';
 import { Loading } from 'notiflix';
 import { GoalService } from 'src/app/services/goal/goal.service';
+import { FormsModule } from '@angular/forms';
+import { FilterGoalModalComponent } from "src/app/components/filter-goal-modal/filter-goal-modal.component";
+import { EditGoalModalComponent } from "src/app/components/edit-goal-modal/edit-goal-modal.component";
+import { GoalFilters } from 'src/app/interfaces/goalFilters.interface';
 
 @Component({
   selector: 'app-goals',
   templateUrl: './goals.page.html',
   styleUrls: ['./goals.page.scss'],
   standalone: true,
-  imports: [IonContent, MenuComponent, CommonModule, HeaderComponent, CreateGoalModalComponent, GoalCardComponent],
+  imports: [IonContent, MenuComponent, CommonModule, HeaderComponent, CreateGoalModalComponent, GoalCardComponent, FormsModule, FilterGoalModalComponent, EditGoalModalComponent],
   animations: [
     trigger('fadeInOut', [
       transition(':enter', [
@@ -33,25 +37,122 @@ import { GoalService } from 'src/app/services/goal/goal.service';
 export class GoalsPage {
 
   public showGoalModal = false;
+  public showFilterModal = false;
+  public showEditGoalModal = false;
+
+  public goalToEdit: any = null;
+
   public userService = inject(UserService);
   public goalService = inject(GoalService);
   public loading: boolean = true;
   public hasGoals: boolean = false;
-  public goals: any[] = [];
+
+  public searchText: string = '';
+  public goals: GoalData[] = [];
+  public filteredGoals: GoalData[] = [];
+  public activeFilters: GoalFilters = {
+    categories: ['all'],
+    goalTypes: {
+      unit: false,
+      habit: false,
+      yes_no: false
+    },
+    statuses: {
+      in_progress: false,
+      completed: false,
+      not_completed: false,
+      cancelled: false
+    },
+    deadlines: {
+      hasDeadline: false,
+      overdue: false,
+      dueThisWeek: false
+    },
+    sortBy: 'created-newest'
+  }
+
 
   public openModal() {
     this.showGoalModal = true;
   }
 
-  public closeModal() {
-    this.showGoalModal = false;
+  public openFilterModal() {
+    this.showFilterModal = true
   }
 
-  public tabs = ['Em andamento', 'Concluído', 'Falhado']
+  public closeModal() {
+    this.showGoalModal = false;
+    this.showFilterModal = false;
+    this.showEditGoalModal = false;
+
+    this.loadGoals()
+  }
+  public openEditGoalModal(goal: GoalData) {
+    this.goalToEdit = goal;
+    this.showEditGoalModal = true;
+  }
+
+  async ionViewWillEnter() {
+    this.loadGoals();
+  }
+
   constructor() { }
 
   async ngOnInit() {
+
     this.loadGoals()
+    this.goalService.checkGoalsForHabit()
+  }
+
+  deleteGoal(goal: GoalData) {
+    try {
+      Swal.fire({
+        title: "Tem certeza?",
+        text: "Você perderá tudo relacionado a meta excluída!",
+        icon: "warning",
+        heightAuto: false,
+        showCancelButton: true,
+        confirmButtonColor: "#1976d2",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sim, desejo deletar."
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.goalService.deleteGoal(goal.id);
+          this.goals = this.goals.filter(g => g.id !== goal.id);
+          this.loadGoals()
+
+          Swal.fire({
+            title: 'Excluido',
+            text: 'Meta excluída com sucesso',
+            icon: 'success',
+            heightAuto: false,
+            confirmButtonColor: '#E0004D'
+          });
+          Loading.remove()
+        }
+      });
+    } catch (err: unknown) {
+      Loading.remove()
+      if (err instanceof Error) {
+        console.error(err);
+        Swal.fire({
+          title: 'Erro',
+          text: 'Não foi possivel excluir a meta',
+          icon: 'error',
+          heightAuto: false,
+          confirmButtonColor: '#E0004D'
+        });
+      } else {
+        console.error('Erro desconhecido', err);
+        Swal.fire({
+          title: 'Erro',
+          text: 'Ocorreu um erro desconhecido',
+          icon: 'error',
+          heightAuto: false,
+          confirmButtonColor: '#E0004D'
+        });
+      }
+    }
   }
 
   async loadGoals() {
@@ -61,68 +162,39 @@ export class GoalsPage {
       if (!uid) throw new Error('Usuário não autenticado');
 
       this.goals = await this.goalService.getUserGoals();
-      console.log(this.goals)
-      this.hasGoals = this.goals.length > 0;
+      this.filteredGoals = this.goalService.filterGoals(this.goals, this.activeFilters, this.searchText);
+
+      this.hasGoals = this.filteredGoals.length > 0;
       this.loading = false;
     } catch {
       this.goals = [];
+      this.filteredGoals = [];
+      this.hasGoals = false;
+      this.loading = false;
     }
   }
 
-  async progressGoalInput(goal: GoalData): Promise<number | null> {
-    const maxAllowed = goal.targetValue! * 100;
 
-    const { value: inputValue } = await Swal.fire({
-      title: 'Registrar progresso',
-      heightAuto: false,
-      text: `Informe seu progresso na meta: "${goal.name}"`,
-      input: 'number',
-      inputAttributes: {
-        min: '0',
-        max: maxAllowed < 100000 ? String(maxAllowed) : '100000',
-        placeholder: 'Ex: 3'
-      },
-      showCancelButton: true,
-      confirmButtonText: 'OK',
-      cancelButtonText: 'Cancelar',
 
-      inputValidator: (value) => {
-        const num = Number(value);
-        if (isNaN(num) || num <= 0 || num > maxAllowed) {
-          return 'Insira um valor válido.';
-        }
-        return null;
+  onFiltersApplied(filters: GoalFilters) {
+    this.activeFilters = filters;
+    this.filteredGoals = this.goalService.filterGoals(this.goals, filters, this.searchText);
+    return this.activeFilters
+  }
+  onSearchChange() {
+    this.filteredGoals = this.goalService.filterGoals(this.goals, this.activeFilters, this.searchText);
+  }
 
-      },
-      didOpen: () => {
-        const input = document.querySelector('.swal2-input');
-        if (input) input.classList.add('swal2-input-wide');
-      }
-    });
-    if (inputValue === undefined) return null;
-    return Number(inputValue);
+  restoreGoal(goal: GoalData) {
+    this.goalService.restoreGoal(goal)
+  }
+
+  completeGoal(goal: GoalData) {
+    this.goalService.completeGoal(goal)
+  }
+  cancelGoal(goal: GoalData) {
+    this.goalService.cancelGoal(goal)
   }
 
 
-
-
-  async completeGoal(goal: GoalData) {
-    const uid = await this.userService.getUserId();
-    if (!uid) return;
-    const inputValue = await this.progressGoalInput(goal);
-    if (inputValue == null) return;
-
-    const target = goal.targetValue ?? 0;
-    let progressValue = inputValue + goal.progressValue;
-    const state: GoalData['state'] = progressValue >= target ? 'completed' : 'in_progress';
-
-
-    await this.goalService.updateGoalProgress(goal.id, {
-      state,
-      progressValue,
-    }, uid);
-
-    goal.state = state;
-    goal.progressValue = progressValue;
-  }
 }
