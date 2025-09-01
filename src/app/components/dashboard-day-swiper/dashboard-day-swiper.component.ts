cf8810ec4a96b122ac79d7f6b95ca8710da84198
Matheus <mgabryel2007@gmail.com>
2025-09-01 16:04:10 -0300

    feat: improvement dashboard-day component with markHabit method and reading logs

--- src/app/components/dashboard-day-swiper/dashboard-day-swiper.component.ts
+++ src/app/components/dashboard-day-swiper/dashboard-day-swiper.component.ts
@@ -1,12 +1,15 @@
+import { GoalService } from 'src/app/services/goal/goal.service';
 import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, Input, OnInit, Output } from '@angular/core';
-import { HabitLog } from 'src/app/interfaces/habitlog.interface';
-import { UserService } from 'src/app/services/user.service';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
 import { registerLocaleData } from '@angular/common';
 import { register } from 'swiper/element/bundle';
 import localePt from '@angular/common/locales/pt';
 import { PREDEFINED_CATEGORIES } from 'src/assets/data/categories';
 import { Category } from 'src/app/interfaces/category.interface';
+import { HabitService } from 'src/app/services/habit/habit.service';
+import { HabitData } from 'src/app/interfaces/habit.interface';
+import { normalizeFirestoreDate } from 'src/app/shared/utils/timestamp.utils';
 registerLocaleData(localePt);
 register()
 
@@ -22,6 +25,8 @@ export class DashboardDaySwiperComponent implements OnInit {
   public habits: any[] = [];
   public logs: any
   public userService = inject(UserService);
+  public habitService = inject(HabitService)
+  public goalService = inject(GoalService)
   public filteredHabits: any[] = [];
   public allweek: any[] = [];
   public currentWeekStart!: Date;
@@ -30,14 +35,20 @@ export class DashboardDaySwiperComponent implements OnInit {
   public showFullHabits = false;
   public isClosing = false;
   public todayIso: any = new Date().toISOString().split('T')[0];
+  public logsByDate: Record<string, Record<string, any>> = {};
 
 
 
   async ngOnInit() {
     this.generateWeek();
     await this.loadHabits();
-    this.updateFilteredHabits();
     this.isLoaded = true;
+    this.logs = await this.habitService.getLogsWithHabit()
+
+
+    console.log(this.logs)
+    this.getLogsForHabits()
+
   }
 
   generateWeek(baseDate: Date = new Date()) {
@@ -52,19 +63,28 @@ export class DashboardDaySwiperComponent implements OnInit {
     }
   }
 
-  async loadLogsForHabitToday(habitId: string) {
-    const uid = await this.userService.getUserId();
-    if (!uid) return;
 
-    const dates = await this.selectedDate
 
-    this.logs = await this.userService.getHabitLogsForDates(uid, habitId, dates);
-    console.log(this.logs)
+  getLogsForHabits() {
+    const habitsLogs = this.logs
+    this.logsByDate = {};
+
+    for (const habit of habitsLogs) {
+      for (const log of habit.logs) {
+        if (!this.logsByDate[log.date]) {
+          this.logsByDate[log.date] = {};
+        }
+
+        this.logsByDate[log.date][habit.id] = log;
+      }
+    }
+    
   }
 
-    getTodayState(): string {
-    const todayLog = this.logs?.[this.selectedDate];
-    return todayLog?.state || 'in_progress';
+  getDayState(habitId: string, date: Date): string {
+    const dateKey = date.toISOString().split('T')[0];
+    const dayLog = this.logsByDate?.[dateKey]?.[habitId];
+    return dayLog?.state || 'in_progress';
   }
 
 
@@ -98,28 +118,44 @@ export class DashboardDaySwiperComponent implements OnInit {
   async loadHabits() {
     const uid = await this.userService.getUserId();
     if (!uid) throw new Error('Usuário não autenticado');
-    this.habits = await this.userService.getUserHabits(uid);
+    this.habits = await this.habitService.getUserHabits();
   }
 
   onDateSelected(date: Date) {
     this.selectedDate = date;
-    this.updateFilteredHabits();
   }
 
+  getHabitsForWeekday(date: Date): any[] {
+    const weekday = this.getWeekday(date);
 
-  updateFilteredHabits() {
-
-    const weekday = this.getWeekday(this.selectedDate);
-    this.filteredHabits = this.habits
-      .filter(habit =>
-        habit.days.map((day: string) => day.toLowerCase()).includes(weekday)
-      )
+    const habitsForDay = this.habits
+      .filter(habit => habit.days.map((d: string) => d.toLowerCase()).includes(weekday) && normalizeFirestoreDate(habit.createdAt) < date)
       .map(habit => ({
         ...habit,
         categoryObj: this.getCategory(habit.category)
       }));
+    return habitsForDay;
   }
 
+  isPastOrToday(date: Date): boolean {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    const compareDate = new Date(date);
+    compareDate.setHours(0, 0, 0, 0); 
+    return compareDate <= today;
+  }
+
+  async markHabit(habit: HabitData, date: Date) {
+    if (!this.isPastOrToday(date)) return;
+    const dateKey = date.toISOString().split('T')[0];
+    const newState = await this.habitService.completeHabit(habit, dateKey);
+    this.logsByDate[dateKey][habit.id] = {
+      ...this.logsByDate[dateKey][habit.id],
+      state: newState,
+    };
+  }
+
+  
 
   getWeekday(date: Date): string {
     return date.toLocaleDateString('pt-BR', { weekday: 'short' }).replace('.', '').toLowerCase();
